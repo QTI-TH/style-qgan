e7c80046df8c7366bdb6516477f7b6d47e94474c
afrancis-heplat <afrancis.heplat@gmail.com>
2021-04-29 12:46:05 +0200

    cleaned up a bit and separated out the qran into a new folder

---
+++ qran/README.md
@@ -0,0 +1,79 @@
+[qgenerator.nl2.ns400.qlassi.nl2.ns2000.pdf](https://github.com/scarrazza/qgmc/files/6351971/qgenerator.nl2.ns400.qlassi.nl2.ns2000.pdf)
+# qgmc - qlassifier
+
+- **Discriminator (D) development:** 
+  - single qubit "wire" to output 0 or 1. 
+  - use re-uploading for this task
+
+- **Generator (G) development:** 
+  - single qubit "wire", 
+  - task is to transform input=(x,y) to output=(x,y_new), where y_new is in the target distribution. 
+  - the discriminator evaluates the result by asigning labels to the y_new. The cost function is minimised when all labels are 1=in the distribution
+
+
+The initial upload is the tutorial implementation of the re-uploading classifier in qibo. Based on this we should be able to learn how to classify also other scenarios and to write our own code to perform the task.
+
+
+# Code info
+
+- **_a.py: First implementation aiming to classify and sample in a Gaussian.**
+  - simplified the tutorial code
+  - **create_dataset** with grid=None creates a set of points with x=[-xwindow,xwindow] and y=[0,1]
+  - **create_target(gauss)** will read the random points and if one of them is within cutoff of a Gaussian distribution with parameters (m,sig) will set the label to 1 and 0 otherwise.
+  - **create_target(gauss2)** creates a more balanced training set: nratio sets the ratio of 0 vs. 1 labels in the training, setting it e.g. to 0.5 means the data is arranged in such a way that half the samples have label 0 will the remainder is within a Gaussian tube defined by (m,sig,cutoff).
+  - running on a laptop is fairly slow, tests with 4 layers already take a long time. It'd be good to run this on a gpu or so. I couldn't port it to google colab easily though. Does anybody have good experience porting simple .py to ipynb?
+   
+   
+# Some results for the discriminator/generator combination
+
+**Full network**  
+Generator data (orange): n_layers=2, n_input=400, cma algorithm
+Discriminator quality (blue): n_layers=2, n_input=2000, cma algorithm
+
+<img width="649" alt="our-gan" src="https://github.com/scarrazza/qgmc/files/6351971/qgenerator.nl2.ns400.qlassi.nl2.ns2000.pdf">
+
+
+Full network: Generator data (orange), Discriminator quality (blue), n_layers=2, n_samples=100, cma algorithm
+
+<img width="649" alt="our-gan" src="https://github.com/scarrazza/qgmc/files/6351036/qgenerator.nl2.ns100.pdf">
+
+n_layers=2, n_samples=400, cma algorithm
+
+<img width="649" alt="our-gan" src="https://github.com/scarrazza/qgmc/files/6351177/qgenerator.nl2.ns400.pdf">
+
+Discriminator alone: n_layers=2, n_samples=200, scipy minimise algorithm
+
+<img width="649" alt="our-gan" src="https://github.com/scarrazza/qgmc/files/6351035/qlassifier.nl2.ns200.nr05.cut01.pdf">
+   
+   
+# Some results for the discriminator
+
+- Gaussian parameters: m=0, sig=0.5, cutoff=0.1, n_samples=200, n_predict=100, nlayers=4
+<img width="649" alt="our-gan" src="https://github.com/scarrazza/qgmc/files/6342777/qlassifier.nl4.ns200.nr05.pdf">
+
+<!--
+- Gaussian parameters: m=0, sig=0.5, cutoff=0.05, n_samples=200, n_predict=100, nlayers=4
+<img width="649" alt="our-gan" src="https://github.com/scarrazza/qgmc/files/6342876/qlassifier.nl4.ns200.nr05.cut005.pdf">
+-->
+<!--
+- Gaussian parameters: m=0, sig=0.5, cutoff=0.05, n_samples=200, n_predict=100, nlayers=5
+<img width="649" alt="our-gan" src="https://github.com/scarrazza/qgmc/files/6343303/qlassifier.nl5.ns200.nr05.cut005.pdf">
+-->
+<!--
+- Gaussian parameters: m=0, sig=0.5, cutoff=0.1, n_samples=400, n_predict=100, nlayers=4
+<img width="649" alt="our-gan" src="https://github.com/scarrazza/qgmc/files/6342776/qlassifier.nl4.ns400.nr05.pdf">
+-->
+<!--
+- Gaussian parameters: m=0, sig=0.5, cutoff=0.1, n_samples=400, n_predict=100, nlayers=5
+<img width="649" alt="our-gan" src="https://github.com/scarrazza/qgmc/files/6343458/qlassifier.nl5.ns400.nr05.cut005.pdf">
+-->
+
+
+# Ressources
+
+## Re-uploading
+
+- [Re-uploading paper](https://arxiv.org/abs/1907.02085)
+
+- [Re-uploading example in qibo](https://qibo.readthedocs.io/en/stable/tutorials/reuploading_classifier/README.html)
+
---
+++ qran/datasets.py
@@ -0,0 +1,167 @@
+# ###############################################################################
+#
+# File datasets.py
+#
+# Copyright (C) 2021 Anthony Francis
+#
+# This software is distributed under the terms of the GNU General Public
+# License (GPL)
+#
+# Routines to generate training data sets. Currently implemented:
+#   create_dataset: Create uniformly distributed random points in x=(x_1,x_2) and call subroutine to add label (y), output is tuple (x,y)
+#   create_target: Creates target array of complex 1+i0 and 0+i1
+#   uniform: Subroutine that creates uniformly distributed random real labels, needed for qgenerator
+#   gauss: Subroutine to imprint labels on training set according to a given Gaussian distribution.
+#   gauss2: Subroutine to imprint labels on training set according to a given Gaussian distribution, where a fixed percentage is inside  the Gaussian tube
+#
+# 
+# Code based on qibo qlassifier tutorial found at 
+# https://qibo.readthedocs.io/en/stable/tutorials/reuploading_classifier/README.html
+# Authors: Adrian Perez Salinas, Stefano Carrazza, Stavros Efthymiou
+# Distributed under the apache 2 license.
+#
+# ###############################################################################
+import numpy as np
+from itertools import product
+
+
+def create_dataset(name, grid=None, samples=200, seed=0):
+    """Function to create training and test sets for classifying.
+    Args:
+        name (str): Name of the problem to create the dataset, to choose between
+            ['gauss', 'circle'].
+        grid (int): Number of points in one direction defining the grid of points.
+            If not specified, the dataset does not follow a regular grid.
+        samples (int): Number of points in the set, randomly located.
+            This argument is ignored if grid is specified.
+        seed (int): Random seed
+    Returns:
+        Dataset for the given problem (x, y)
+    """
+    if grid == None:
+        np.random.seed(seed)
+        xwindow=1
+        x = xwindow * ( 1 - 2 * np.random.rand(samples, 1)) # between -xwindow and xwindow
+        y = np.random.rand(samples, 1) # between 0 and 1
+        points = np.concatenate((x, y), axis=1)
+    
+    else:
+        x = np.linspace(-1, 1, grid)
+        points = np.array(list(product(x, x)))
+        
+        
+    creator = globals()[f"_{name}"]
+    x, y = creator(points)
+    return x, y
+
+
+def create_target(name):
+    """Function to create target states for classification.
+    Args:
+        name (str): Name of the problem to create the target states, to choose between
+            ['circle', '3 circles', 'square', '4 squares', 'crown', 'tricrown', 'wavy lines']
+    Returns:
+        List of numpy arrays encoding target states that depend only on the number of classes of the given problem
+    """
+    if name in ['gauss', 'gauss2']:
+        targets = [np.array([1, 0], dtype='complex'),
+                   np.array([0, 1], dtype='complex')]
+    elif name in ['uniform']:
+        targets = [np.array([1, 0], dtype='complex'),
+                   np.array([0, 1], dtype='complex')]
+ 
+    #print(targets)
+ 
+    return targets
+    
+
+# output is uniformly distributed numbers    
+def _uniform(points):
+    labels = np.zeros(len(points), dtype=np.int32)
+    labels = points[:,1]
+        
+    return points, labels
+    
+
+# points from the randomly chosen grid withing distance cutoff of the target Gaussian are flagges with label 1. 
+# Here there is naturally an imbalance between 0 and 1 labels    
+def _gauss(points):
+      
+    def gaussian(x,m,sig):
+        norm = 0.9 # puts whole range into random grid ranges, should be this: 1/(sig * np.sqrt(2*np.pi)) but we want it between 0 and 1
+        res = norm * np.exp(-0.5 * ((x-m)/sig)**2 )
+        return res
+    
+    def target_marker(x,m,sig,y,cutoff):
+        test = gaussian(x,m,sig) - y
+        if(np.abs(test) < cutoff):
+            res=1
+        else:
+            res=0
+        return res    
+         
+    # some parameters       
+    m=0
+    sig=0.5
+    cutoff=0.1
+    
+    #print(points)   
+    labels = np.zeros(len(points), dtype=np.int32)
+        
+    for n in range(0,len(points)):
+        x=points[n,0]
+        y=points[n,1]
+        
+        #print(x,y,gaussian(x,m,sig),target_marker(x,m,sig,y,cutoff))
+        labels[n]=target_marker(x,m,sig,y,cutoff)
+        
+    return points, labels   
+    
+    
+# In this version we prepare a dataset that has as many 0 as 1 label points    
+def _gauss2(points):
+      
+    def gaussian(x,m,sig):
+        norm = 0.9 # puts whole range into random grid ranges, should be this: 1/(sig * np.sqrt(2*np.pi)) but we want it between 0 and 1
+        res = norm * np.exp(-0.5 * ((x-m)/sig)**2 )
+        return res
+    
+    def target_marker(x,m,sig,y,cutoff):
+        test = gaussian(x,m,sig) - y
+        if(np.abs(test) < cutoff):
+            res=1
+        else:
+            res=0
+        return res    
+         
+    # some parameters       
+    m=0
+    sig=0.5
+    cutoff=0.1 # defines width of Gaussian tube
+    nratio=0.33 # controls the reatio between 0 and 1 labels, 1=random, 0=all on Gauss tube
+    
+    #print(points)   
+    labels = np.zeros(len(points), dtype=np.int32)
+    
+    count=0    
+    for n in range(0,len(points)):
+        x=points[n,0]
+        y=points[n,1]
+        
+        # check label first, if it's zero and there's less than len(points)/2 of them, take it
+        tag=target_marker(x,m,sig,y,cutoff)
+        if tag == 0 and count<int(len(points)*nratio):
+            labels[n]=tag
+            count+=1
+        elif tag == 1: # keep the point if it's got a label in the Gaussian tube already
+            labels[n]=tag 
+        else: # create a random point at x on within the Gaussian tube
+            y_new=gaussian(x,m,sig) + cutoff * ( 1 - 2 * np.random.rand(1, 1))   
+            points[n,1]=y_new
+            labels[n]=1
+        
+        
+    return points, labels       
+     
+
+
