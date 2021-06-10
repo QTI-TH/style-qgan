b934287f196e9710df80c7471487284bed998962
bpcarlos <charl_ap@hotmail.es>
2021-06-10 11:28:14 +0200

    Add files via upload
    Fixed some errors

--- classical-quantum gan/quantum-classical_LHCdata.py
+++ classical-quantum gan/quantum-classical_LHCdata.py
@@ -47,7 +47,7 @@ def define_discriminator(n_inputs=3, alpha=0.2, dropout=0.2):
     model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
     return model
 
-def rotate(theta, x):
+def rotate(theta, x, latent_dim, layers, nqubits):
     theta = tf.reshape(theta, (latent_dim + 1, 4*layers*nqubits + nqubits + 4*layers))
     return tf.matmul(x, theta[1:]) + theta[0]
  
@@ -63,7 +63,7 @@ def define_cost_gan(params, discriminator, latent_dim, samples):
     loss = tf.reduce_mean(loss)
     return loss
 
-def load_events(filename):
+def load_events(filename, real_samples):
     init = readInit(filename)
     evs = list(readEvent(filename))
 
@@ -81,7 +81,7 @@ def load_events(filename):
     return scaler.transform(pt.transform(invar[:real_samples, :]))
  
 # generate real samples with class labels
-def generate_real_samples(samples, distribution):
+def generate_real_samples(samples, distribution, real_samples):
     # generate samples from the distribution
     idx = np.random.randint(real_samples, size=samples)
     X = distribution[idx,:]
@@ -90,21 +90,21 @@ def generate_real_samples(samples, distribution):
     return X, y
 
 # define hamiltonian to generate fake samples
-def hamiltonian1():
+def hamiltonian1(nqubits):
     id = [[1, 0], [0, 1]]
     m0 = hamiltonians.Z(1, numpy=True).matrix
     m0 = np.kron(id, np.kron(id, m0))
     ham = hamiltonians.Hamiltonian(nqubits, m0)
     return ham
 
-def hamiltonian2():
+def hamiltonian2(nqubits):
     id = [[1, 0], [0, 1]]
     m0 = hamiltonians.Z(1, numpy=True).matrix
     m0 = np.kron(id, np.kron(m0, id))
     ham = hamiltonians.Hamiltonian(nqubits, m0)
     return ham
 
-def hamiltonian3():
+def hamiltonian3(nqubits):
     id = [[1, 0], [0, 1]]
     m0 = hamiltonians.Z(1, numpy=True).matrix
     m0 = np.kron(m0, np.kron(id, id))
@@ -120,7 +120,7 @@ def generate_latent_points(latent_dim, samples):
     return x_input
  
 # use the generator to generate fake examples, with class labels
-def generate_fake_samples(params, latent_dim, samples):
+def generate_fake_samples(params, latent_dim, samples, layers, nqubits):
     # generate points in latent space
     x_input = generate_latent_points(latent_dim, samples)
     # quantum generator circuit
@@ -139,12 +139,12 @@ def generate_fake_samples(params, latent_dim, samples):
     X1 = []
     X2 = []
     X3 = []
-    newparams = rotate(params, x_input)
+    newparams = rotate(params, x_input, latent_dim, layers, nqubits)
     for newparams_i in newparams:
         circuit.set_parameters(newparams_i)
-        X1.append(hamiltonian1().expectation(circuit.execute()))
-        X2.append(hamiltonian2().expectation(circuit.execute()))
-        X3.append(hamiltonian3().expectation(circuit.execute()))
+        X1.append(hamiltonian1(nqubits).expectation(circuit.execute()))
+        X2.append(hamiltonian2(nqubits).expectation(circuit.execute()))
+        X3.append(hamiltonian3(nqubits).expectation(circuit.execute()))
     # shape array
     X = tf.stack((X1, X2, X3), axis=1)
     # create class labels
@@ -152,7 +152,7 @@ def generate_fake_samples(params, latent_dim, samples):
     return X, y
  
 # train the generator and discriminator
-def train(d_model, latent_dim, n_epochs=30000, samples=128):
+def train(d_model, latent_dim, layers, nqubits, real_samples, discriminator, n_epochs=30000, samples=128):
     d_loss = []
     g_loss = []
     # determine half the size of one batch, for updating the discriminator
@@ -160,13 +160,13 @@ def train(d_model, latent_dim, n_epochs=30000, samples=128):
     initial_params = tf.Variable(np.random.uniform(0, 2*np.pi, (latent_dim+1)*(4*layers*nqubits + nqubits + 4*layers)))
     optimizer = tf.optimizers.Adadelta(lr=0.1)
     # prepare real samples
-    s = load_events('data/ppttbar_10k_events.lhe')
+    s = load_events('data/ppttbar_10k_events.lhe', real_samples)
     # manually enumerate epochs
     for i in range(n_epochs):
         # prepare real samples
-        x_real, y_real = generate_real_samples(half_samples, s)
+        x_real, y_real = generate_real_samples(half_samples, s, real_samples)
         # prepare fake examples
-        x_fake, y_fake = generate_fake_samples(initial_params, latent_dim, half_samples)
+        x_fake, y_fake = generate_fake_samples(initial_params, latent_dim, half_samples, layers, nqubits)
         # update discriminator
         d_loss_real, _ = d_model.train_on_batch(x_real, y_real)
         d_loss_fake, _ = d_model.train_on_batch(x_fake, y_fake)
@@ -187,7 +187,7 @@ def main(nqubits, latent_dim, layers, real_samples):
     # create the discriminator
     discriminator = define_discriminator()
     # train model
-    train(discriminator, latent_dim)
+    train(discriminator, latent_dim, layers, nqubits, real_samples, discriminator)
 
 
 if __name__ == "__main__":
@@ -195,6 +195,6 @@ if __name__ == "__main__":
     parser.add_argument("--nqubits", default=3, type=int)
     parser.add_argument("--latent_dim", default=1, type=int)
     parser.add_argument("--layers", default=5, type=int)
-    parser.add_argument("--real_samples", default=10000, type=float)
+    parser.add_argument("--real_samples", default=10000, type=int)
     args = vars(parser.parse_args())
     main(**args)
